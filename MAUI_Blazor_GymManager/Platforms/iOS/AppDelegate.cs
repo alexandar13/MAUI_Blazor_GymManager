77f94ce9b2d0e72171fabf30421b49ef5e64c303
aleksandar.radosavljevic@fazi.rs <aleksandar.radosavljevic@fazi.rs>
2025-05-26 23:48:18 +0200

    Base folder removed

---
+++ MAUI_Blazor_GymManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_Blazor_GymManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ Services/Interfaces/INavigationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface INavigationService
+    {
+        Func<string, Task>? NavigateToMauiPageAction { get; set; }
+        Task NavigateToMauiPage(string pageRoute);
+    }
+}
